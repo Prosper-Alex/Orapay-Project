ac63dfc6ce2693e31ba4f53449c07f28ebc7ae38
Prosper Alex <chigoziealex011@gmail.com>
2025-09-23 06:04:14 +0100

    Add navigation toggle functionality to enhance user interaction

--- js/script.js
+++ js/script.js
@@ -16,3 +16,11 @@ function startCounter(elementId, end, duration, suffix = "") {
 startCounter("counter1", 10, 2000); // count 0 → 30 in 2s
 startCounter("counter2", 30, 2000); // count 0 → 50 in 3s
 startCounter("counter3", 100, 4000); // count 0 → 100 in 4s
+
+// navigation
+
+const navToggle = document.getElementById("navToggle");
+
+navToggle.addEventListener("click", () => {
+  navToggle.classList.toggle("active");
+});
